45f88bbfa277bcaf909317745f00b3d3c0d29154
shungo0107 <odaira.sh@icloud.com>
2024-09-29 16:03:52 +0900

    課題提出のため

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,15 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		Car_Chapter15 displaySpeed = new Car_Chapter15();
+		
+		displaySpeed.gearChange(3);
+
+		displaySpeed.run();
+		
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,22 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+    private int gear = 1;
+	private int speed = 10;     
+
+	public void gearChange ( int afterGear) {
+		this.speed = switch (afterGear) {
+		    case 1 ->  10;
+		    case 2 ->  20;
+		    case 3 ->  30;
+		    case 4 ->  40;
+		    case 5 ->  50;
+            default -> 10;	    		
+	    };
+	}
+	
+	public void run () {
+		System.out.println(speed);	
+	}
+}
